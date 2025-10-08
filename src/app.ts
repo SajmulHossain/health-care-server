09576691319a6eae15281843d249acb092a20329
Sajmul <sajmul1427@gmail.com>
2025-10-08 22:44:42 +0600

    prisma setup done

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,30 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import notFound from './app/middlewares/notFound';
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+import config from "./config";
+import { uptime } from "process";
 
 const app: Application = express();
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/', (req: Request, res: Response) => {
-    res.send({
-        Message: "Ph health care server.."
-    })
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "Server is running..",
+    environment: config.node_env,
+    uptime: uptime().toLocaleString() + "seconds",
+    timeStamps: new Date().toLocaleString(),
+  });
 });
-
 
 app.use(globalErrorHandler);
 
